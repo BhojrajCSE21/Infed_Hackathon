adc95bafe75ac93feb3b1ee1b24c4dbc497c74c7
Harshal Honde <105191619+harshalhonde21@users.noreply.github.com>
2024-08-16 17:26:28 +0530

    updated

--- Backend/configs.js
+++ Backend/configs.js
@@ -1,4 +1,4 @@
 export default {
-  mongoURI: 'mongodb://localhost:27017/careerCompany',
+  mongoURI: 'mongodb+srv://harshalhonde17:harshal172003@cluster0.b0mwyen.mongodb.net/Blogs?retryWrites=true&w=majority',
   jwtSecret: '65984323245965!@$#^&*&44546451564',
 };
